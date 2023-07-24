145dd2660b39eff32124df08396c651f47c0d988
tufaelkhan <tufaelkhan247@gmail.com>
2023-07-24 08:09:31 -0700

    add review

--- src/Components/Home/Allcollege/Profile.jsx
+++ src/Components/Home/Allcollege/Profile.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { FaAddressCard, FaBook, FaBuilding, FaEdit, FaPhoneAlt, FaUserAlt } from 'react-icons/fa';
 
 const Profile = () => {
     const [profiles, setProfiles] = useState([])
@@ -18,13 +19,13 @@ const Profile = () => {
             {
                 profiles.map((item) =>  <div key={item._id} className="card w-96 bg-base-100 shadow-xl">
                 <div className="card-body">
-                    <p className='text-2xl font-bold'>{item.name}</p>
-                    <p className='text-2xl font-bold'>{item.college}</p>
-              <p className='text-2xl font-bold'>Subject: {item.subject}</p>
-              <p className='text-2xl font-bold'>Address: {item.address}</p>
-              <p className='text-2xl font-bold'>Phone: {item.phone}</p>
+                    <p className='text-2xl font-bold flex'><span><FaUserAlt/> </span> {item.name}</p>
+                    <p className='text-1xl font-bold flex items-center'><span className='mr-2'><FaBuilding/></span> {item.college}</p>
+              <p className='text-2xl font-bold flex items-center'><span className='mr-2'><FaBook/></span> {item.subject}</p>
+              <p className='text-2xl font-bold flex items-center'><span className='mr-2'><FaAddressCard/></span> {item.address}</p>
+              <p className='text-2xl font-bold flex items-center'><span className='mr-2'><FaPhoneAlt/></span> {item.phone}</p>
                     <div className="card-actions justify-end">
-                        <Link to={`/profile/${item._id}`}><button className="btn btn-primary">Edit profile</button></Link>
+                        <Link to={`/profile/${item._id}`}><button className="btn btn-primary">Profile<FaEdit/> </button></Link>
                     </div>
                 </div>
             </div>)
--- src/Components/Home/Home/HappyStudents.jsx
+++ src/Components/Home/Home/HappyStudents.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
+import { FaBeer } from 'react-icons/fa';
 
 const HappyStudents = () => {
     return (
-        <div className='mt-10 mb-10 bg-base-200 pt-8 pb-9'>
+        <div className='mt-10 mb-10 bg-base-200 pt-8 pb-9 md:ml-10'>
         <div className='text-center mb-6'>
         
-                    <h3 className='text-blue-600 font-bold'>Happy Students</h3>
+                    <h3 className='text-blue-600 font-bold'> Students Reviews</h3>
                     <h3><span className='text-blue-950 font-extrabold text-2xl'>Our Students</span><span className='text-blue-500 font-bold'> Says</span></h3>
         </div>
-                <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 '>
-                <div className="card w-96 bg-base-100 shadow-xl">
+                <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
+                <div className="card w-96 bg-base-100 shadow-xl my-3">
   <div className="card-body">
+    <p className='text-2xl text-blue-500 font-bold'>United States institute of Peace</p>
     <p>Rating:</p>
     <p>Informative, user-friendly, and visually appealing. Offers a wide range of educational resources for various subjects and levels. Highly recommended!</p>
     <div className='flex mt-5'>
@@ -22,8 +24,9 @@ const HappyStudents = () => {
     </div>
   </div>
 </div>
-                <div className="card w-96 bg-base-100 shadow-xl">
+                <div className="card w-96 bg-base-100 shadow-xl my-3">
   <div className="card-body">
+  <p className='text-2xl text-blue-500 font-bold'>Bangladesh skill development institute</p>
     <p>Rating:</p>
     <p>The website's intuitive navigation and interactive features enhance the learning experience. Regular updates ensure up-to-date content. Overall, a valuable platform for lifelong learners.</p>
     <div className='flex mt-5'>
@@ -35,14 +38,57 @@ const HappyStudents = () => {
     </div>
   </div>
 </div>
-                <div className="card w-96 bg-base-100 shadow-xl">
+                <div className="card w-96 bg-base-100 shadow-xl my-3">
   <div className="card-body">
+  <p className='text-2xl text-blue-500 font-bold'>Daffodil International college</p>
     <p>Rating:</p>
     <p>Engaging multimedia content, collaborative community forum, and a dedication to excellence make this education website an exceptional resource for students and educators. Motivating and immersive learning experience.</p>
     <div className='flex mt-5'>
         <img className='w-14 rounded-full mr-9' src="https://images.generated.photos/nngehfxqaZhvgYFQ5D04O5CRwuzXZI6338WTzt9dp7U/rs:fit:256:256/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/MTg3NDQ1LmpwZw.jpg" alt="" />
         <div>
             <p>Rakib khan</p>
+            <p>software developer</p>
+        </div>
+    </div>
+  </div>
+</div>
+                <div className="card w-96 bg-base-100 shadow-xl my-3">
+  <div className="card-body">
+    <p className='text-2xl text-blue-500 font-bold'>Chandpur Technical college</p>
+    <p>Rating:</p>
+    <p>Harmony College has exceeded my expectations in every way. The campus is historic and picturesque, and the academic programs are rigorous and intellectually stimulating. The professors are experts in their fields and go the extra mile to support students' growth and development.</p>
+    <div className='flex mt-5'>
+        <img className='w-14 rounded-full mr-9' src="https://images.generated.photos/jmEVeh_oGdKp1HpW5CwmL90zyzTMywg7jw8WHVBz7gc/rs:fit:256:256/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/NjM5MzQ2LmpwZw.jpg" alt="" />
+        <div>
+            <p>Jahid khan</p>
+            <p>web desiginer</p>
+        </div>
+    </div>
+  </div>
+</div>
+                <div className="card w-96 bg-base-100 shadow-xl my-3">
+  <div className="card-body">
+  <p className='text-2xl text-blue-500 font-bold'>Baburhat College</p>
+    <p>Rating:</p>
+    <p>Azure State University has been a mixed experience for me. The campus itself is stunning, with modern facilities and beautiful green spaces. However, some of the classes can be overcrowded, making it challenging to get individual attention from professors.</p>
+    <div className='flex mt-5'>
+        <img className='w-14 rounded-full mr-9' src="https://images.generated.photos/voQD32s0UL9HheC7j1LXd1iCr6kiVqEThf_haEBQVSM/rs:fit:256:256/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/NTUwOTY1LmpwZw.jpg" alt="" />
+        <div>
+            <p>Rakib khan</p>
+            <p>Accounting</p>
+        </div>
+    </div>
+  </div>
+</div>
+                <div className="card w-96 bg-base-100 shadow-xl my-3">
+  <div className="card-body">
+  <p className='text-2xl text-blue-500 font-bold'>Puran Bazar Digree College</p>
+    <p>Rating:</p>
+    <p>Evergreen University has been an incredible experience for me. The campus is beautiful, and the faculty is top-notch. The professors are approachable and passionate about their subjects, which creates an engaging learning environment.</p>
+    <div className='flex mt-5'>
+        <img className='w-14 rounded-full mr-9' src="https://images.generated.photos/nngehfxqaZhvgYFQ5D04O5CRwuzXZI6338WTzt9dp7U/rs:fit:256:256/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/MTg3NDQ1LmpwZw.jpg" alt="" />
+        <div>
+            <p>sazid khan</p>
             <p>software developer</p>
         </div>
     </div>
